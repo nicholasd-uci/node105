48563675df382a88659282cca9aacbbbd15f891e
Nicholas Dallas <nicholas.dallas0@gmail.com>
2021-01-26 13:18:35 -0800

    index.js and added ex. of constructors

---
+++ index.js
@@ -0,0 +1,12 @@
+// These are Constructors
+let greeting = 'Hello world'
+let greeting = new String('Hello world!')
+
+let num = 5
+let num = new Number(5)
+
+let bool = true
+let bool = new Boolean(false)
+
+
+
