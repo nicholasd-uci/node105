ec2def264347c531757b0c6bf6175d59c92f0c48
Nicholas Dallas <nicholas.dallas0@gmail.com>
2021-01-26 20:19:44 -0800

    updated file

--- index.js
+++ index.js
@@ -8,5 +8,20 @@ let num = new Number(5)
 let bool = true
 let bool = new Boolean(false)
 
+// 1. Original Build For Constructors In JavaScript
+//      - Have To Use "This" In Constructors
+
+function Animal (name, type, age, sound) {
+    this.name = name
+    this.type = type
+    this.age = age
+    this.sound = sound
+}
+
+
+// 2. Factory Function 
+
+// 3. Class Constructors
+
 
 
