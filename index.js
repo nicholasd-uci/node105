f3c826aa22d408faa60cfa65cfa9e9602f7cfc9c
Nicholas Dallas <nicholas.dallas0@gmail.com>
2021-02-02 18:57:53 -0800

    updated class notes for factory function on index.js

--- index.js
+++ index.js
@@ -51,11 +51,40 @@ theRoom.printInfo()
 // Hands you back an 'Object' this method is not considered "Constructors"
 //          - we do no use "this" 
 
-const person = (name, age, email, address) => ({ name, age, email, address })
-
-let john = person('john', 47, 'john@yahoo.com', '123 wouth st' )
-
-console.log(john)
+function Person (name, age, email, address) {
+    this.name = name
+    this.age = age
+    this.email = email
+    this.address = address
+    this.printInfo = function () {
+      console.log(`
+        Name: ${this.name}
+        Age: ${this.age}
+        Email: ${this.email}
+        Address: ${this.address}
+      `)
+    }
+  }
+  
+  function Student (name, age, email, address, grade, gpa) {
+      Person.call(this, name, age, email, address)
+      this.grade = grade
+      this.gpa = gpa
+  }
+  
+  let student = new Student('Jack', 18, 'jack@yahoo.com', '123 south st', 12, 3.8)
+  student.printInfo()
+  console.log(student)
+  
+  function Teacher (name, age, email, address, subject, room) {
+      Person.call(this, name, age, email, address)
+      this.subject = subject
+      this.room = room
+  }
+  
+  let teacher = new Teacher('john', 47, 'john@email.com', '234 west st', 'English', 403)
+  teacher.printInfo()
+  console.log(teacher)
 
 
 // 3. Class Constructors
